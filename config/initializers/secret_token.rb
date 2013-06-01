4475e7b4d591547216f65c2e3d4eaa33081b1c7a
Luis Michel <darkeva02@M11xR2Ubuntu.(none)>
2013-06-01 13:28:11 -0500

    Primer commit del AngelHackDF de ProjectVolunteer

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjectVolunteer::Application.config.secret_token = 'aa527dbda02cb32c1508430068595f4fe719c7d0585b7e3f0b8b1bbbfdc49f71bf3ff0d557b63502d22b717daebc01646eed874dab18dae2398897588b32264e'
