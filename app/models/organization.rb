e0fb6479dee65c8c09e853259b1d5309609705c7
Rene <darktrainer666@gmail.com>
2013-06-01 23:10:16 -0500

    agregar la variable description como accesible en organization

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,5 +1,5 @@
 class Organization < ActiveRecord::Base
-  attr_accessible :address_hq, :id_category, :id_organization, :id_user, :name, :phone_number, :register_date, :website_url
+  attr_accessible :address_hq, :id_category, :id_organization, :id_user, :name, :phone_number, :register_date, :website_url, :description
 
   belongs_to :user
   has_many :campaigns
