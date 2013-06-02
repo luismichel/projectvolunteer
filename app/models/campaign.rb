5bdfe1d98bc4ed3bcf54aea31f1bdc0c68ca1e63
Rene <darktrainer666@gmail.com>
2013-06-02 10:33:42 -0500

    Agregue el migration del avatar a campa~na

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -2,5 +2,5 @@ class Campaign < ActiveRecord::Base
   attr_accessible :date_event, :date_goal, :description, :economic_goal, :id_campaign, :id_category, :id_organization, :location, :man_power_goal, :name, :id_user, :avatar
 
   belongs_to :organization
-  has_attached_file :avatar, :styles => { :normal => "780x400", :small => "390x200", :thumb => "180x100"}, :default_url => "missing_campaign_cover.png"
+  has_attached_file :avatar, :styles => { :normal => "780x400", :small => "390x200", :thumb => "180x100"}, :default_url => ":style/missing_campaign_cover.png"
 end
