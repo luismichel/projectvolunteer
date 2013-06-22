d2866370e53d4d5aa481eaccbf8caed21dbbb2e8
Salvador Elizarraras <salvadorbfm@gmail.com>
2013-06-21 22:32:01 -0500

    Functional sliders for donation have been added

--- app/assets/javascripts/campaign.js
+++ app/assets/javascripts/campaign.js
@@ -1,11 +1,11 @@
-jQuery(function() {
+$(function() {
   $("#btn-donate").bind("click", function(e){
     e.preventDefault();
     $("#dialog-form").dialog("open");
   });
 });
 
-jQuery(function() {
+$(function() {
     $("#dialog-form").dialog({
         autoOpen: false,
         height: 500,
@@ -20,7 +20,75 @@ jQuery(function() {
             $( this ).dialog( "close" )
         },
         close: function(){
-            console.log("Pos aca cerrando mas que nada");
         }
     });
 });
+
+$(function() {
+    var roundDecimal = function( number ){
+        return Math.round( number * 100 ) / 100;
+    };
+    var updateSliders = function(toBeUpdatedSelector, updatedValue){
+        //var updatedValue = parseFloat($(updatedSelector).slider("value"), 10);
+        updatedValue = updatedValue || 0.0;
+        var newValue = (100.00 - updatedValue);
+        $( toBeUpdatedSelector ).slider( "value" , newValue);
+    };
+
+    var updateAmounts = function( from, updatedValue){
+        from = from || "";
+        updatedValue = updatedValue || 0.0;
+        var donationValue = parseFloat($("#amount").val(), 10);
+        var campaignValue = parseFloat($("#slider-range-campaign").slider("value"), 10);
+        var volunteerValue = parseFloat($("#slider-range-volunteer").slider("value"), 10);
+        if (from === "from-campaign") {
+            campaignValue = updatedValue;
+            volunteerValue = 100.00 - updatedValue;
+        }else if (from == "from-volunteer") {
+            campaignValue = 100.00 - updatedValue;
+            volunteerValue = updatedValue;
+        }else{
+            campaignValue = parseFloat($("#slider-range-campaign").slider("value"), 10);
+            volunteerValue = parseFloat($("#slider-range-volunteer").slider("value"), 10);
+        }
+        $("#amount-campaign").html( roundDecimal(donationValue*(campaignValue/100.00)) );
+        $("#amount-volunteer").html( roundDecimal(donationValue*(volunteerValue/100.00)) );
+    };
+
+    $("#amount").bind("change_campaign", function(){
+        updateSliders("#slider-range-campaign", "#slider-range-volunteer");
+        updateAmounts();
+    });
+
+    $("#amount").bind("change_volunteer", function(){
+        updateSliders("#slider-range-volunteer", "#slider-range-campaign");
+        updateAmounts();
+    });
+
+    $( "#slider-range-campaign" ).slider({
+      range: "min",
+      value: 95,
+      min: 0,
+      max: 100,
+      slide: function( event, ui ) {
+        //$("#amount").trigger("change_campaign");
+        updateSliders("#slider-range-volunteer", ui.value);
+        updateAmounts("from-campaign", ui.value);
+      }
+    });
+
+    $( "#slider-range-volunteer").slider({
+      range: "min",
+      value: 5,
+      min: 0,
+      max: 100,
+      slide: function( event, ui ) {
+        //$("#amount").trigger("change_volunteer");
+        updateSliders("#slider-range-campaign" , ui.value);
+        updateAmounts("from-volunteer", ui.value);
+      }
+    });
+
+    $( "#amount" ).val( 25.00 );
+    updateAmounts();
+  });
